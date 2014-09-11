f0f24016eb0597530ac5d718664a96c26eaa5546
David Stoline <drupal@davidstoline.com>
2014-09-11 15:06:08 -0400

    Fully qualified namespace.

--- tests/FakerXssTest.php
+++ tests/FakerXssTest.php
@@ -8,15 +8,15 @@ class FakerXssTest extends \PHPUnit_Framework_TestCase
 {
     public function testXss()
     {
-        $faker = Faker\Factory::create();
-        $faker->addProvider(new FakerXss\Xss($faker));
+        $faker = \Faker\Factory::create();
+        $faker->addProvider(new Xss($faker));
         $xss = $faker->xss;
         $this->assertEquals("<script>alert('XSS');</script>", $xss);
     }
 
     public function testXssCustom()
     {
-        $faker = Faker\Factory::create();
+        $faker = \Faker\Factory::create();
         $faker->addProvider(new Xss($faker));
         $xss = $faker->xss('Stoline');
         $this->assertEquals("<script>alert('Stoline');</script>", $xss);
